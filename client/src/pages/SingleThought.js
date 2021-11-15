0f72cb0473224924584d17a4ada308c7587a234a
kimcc <15278020+kimcc@users.noreply.github.com>
2021-11-14 23:00:49 -0600

    add tadditional form functionality

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -5,6 +5,8 @@ import ReactionList from '../components/ReactionList';
 
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm'
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -34,6 +36,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
